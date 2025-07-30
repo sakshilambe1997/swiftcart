a84b0da8381168f86632ebe0c729da99392a2819
sakshilambe1997 <sakshilambe1997@gmail.com>
2025-07-30 17:07:37 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
