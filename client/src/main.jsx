e7bf7d21009aae8f9174380faa64f21f38ce8b6e
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2025-07-31 19:04:51 +0530

    routing set up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root')) ;
+
+const router = createBrowserRouter([
+
+  {
+    path: "/" ,
+    element: <App/> 
+
+  } ,
+  {
+    path: "*",
+    element : <h1>404 Not Found</h1>
+  }
+])
+root.render(<RouterProvider router={router}/>)
