ad96a6ebd2bf3fdc931fcd63a2b8e124a47739ae
sakshilambe1997 <sakshilambe1997@gmail.com>
2025-08-01 15:06:07 +0530

    some changes added to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar></Navbar>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
