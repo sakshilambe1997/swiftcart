4b43c9c36800e9a7d9c7d9a89717f2d351ca0d00
sakshilambe1997 <sakshilambe1997@gmail.com>
2025-10-10 17:26:47 +0530

    Order Model Created

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,5 +2,56 @@ import { model,Schema} from "mongoose";
 
 const orderSchema = new Schema({
 
+  userId:{
+     type: Schema.Types.ObjectId,
+     ref:"User",
+     required:true
+  },
 
+  products:[{
+
+    productId:{
+     type:Schema.Types.ObjectId,
+    ref:"Product",
+    required:true
+    },
+
+    quantity:{
+        type:Number,
+        required:true
+    },
+
+    price:{
+        type :Number,
+        required:true
+    }
+  }],
+
+  totalBill:{
+    type:Number,
+    required:true
+  },
+  
+  address:{
+    type:String,
+    required:true
+  },
+
+  phoneNumber:{
+    type:String,
+    required:true
+  },
+
+  paymentMode:{
+    enum:["COD", "UPI","Bank Transfer"],
+    required:true
+  },
+
+//  paymentId:{
+//     ref:"Payment",
+//     status:["pending","Delivered","Cancel"]
+//  }
 })
+
+const Order= model("Order",orderSchema);
+export default Order;
